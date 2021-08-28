2f99f0c63db94e864bf9678de9d496d0de312697
hxndlo <hxndlo@hotmail.com>
2021-08-27 22:41:00 -0500

    Corrección

--- Components/tienda/controller.js
+++ Components/tienda/controller.js
@@ -4,18 +4,18 @@ const pool = new Pool ({
     user: "postgres",
     host: "localhost",
     database: "tienda",
-    password: "Aki9618Misa",
-    port: 5432
+    password: "Handlo88",
+    port: 5433
 })
 
 const obtenerCliente = async (req, res) => {
-    const response = await pool.query('SELECT * FROM "U2_TALLER_06".cliente ORDER BY cedula ASC');
+    const response = await pool.query('SELECT * FROM produccion.cliente');
     res.status(200).json(response.rows);
 };
 
 const agregarCliente = async (req, res) => {
     const { cedula, nombre, apellido } = req.body;
-    const response = await pool.query('INSERT INTO "U2_TALLER_06".cliente (cedula, nombre, apellido) VALUES ($1, $2, $3)', [cedula, nombre, apellido]);
+    const response = await pool.query('INSERT INTO produccion.cliente (cedula, nombre, apellido) VALUES ($1, $2, $3)', [cedula, nombre, apellido]);
     res.json({
         message: 'Cliente Agregado con éxito',
         body: {
